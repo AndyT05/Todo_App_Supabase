f179899bdefeef8698bda9011235796f9846fc00
AndyT05 <thanhan22595@gmail.com>
2025-09-26 20:12:32 -0500

    add google signin

---
+++ vite-project/src/pages/SignIn.jsx
@@ -0,0 +1,52 @@
+import supabase from "../supabase-clients";
+import { useEffect, useState } from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useNavigate } from "react-router-dom";
+const SignIn = () => {
+  const [session, setSession] = useState(null);
+  const navigate = useNavigate();
+  const signOut = async () => {
+    await supabase.auth.signOut();
+    setSession(null);
+  };
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+      if (session) {
+        navigate("/todo");
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="max-w-sm w-full p-8 mt-10 bg-white rounded-lg shadow-lg">
+          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>Welcome!</h2>
+        <button
+          onClick={signOut}
+          className="bg-red-500 text-white rounded-md px-4 py-2 hover:bg-red-600 active:bg-red-700 transition duration-200"
+        >
+          Sign out
+        </button>
+      </div>
+    );
+  }
+};
+
+export default SignIn;
---
+++ vite-project/src/pages/Todo.jsx
@@ -0,0 +1,119 @@
+import { useState, useEffect } from "react";
+import supabase from "../supabase-clients";
+
+const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const { data, error } = await supabase.from("todo").select("*");
+    if (error) {
+      console.error("Error fetching todo: ", error);
+    } else {
+      setTodoList(data);
+    }
+  };
+
+  const addTodo = async () => {
+    const newTodoData = {
+      name: newTodo,
+      isCompleted: false,
+    };
+    const { data, error } = await supabase
+      .from("todo")
+      .insert([newTodoData])
+      .select();
+    if (error) {
+      console.error("Error adding todo:", error);
+    } else {
+      setTodoList((prev) => [...prev, data[0]]);
+      setNewTodo("");
+    }
+  };
+
+  const completeTask = async (id, isCompleted) => {
+    const { error } = await supabase
+      .from("todo")
+      .update({ isCompleted: !isCompleted })
+      .eq("id", id);
+    if (error) {
+      console.log("Error toggling task", error);
+    } else {
+      const updatedTodoList = todoList.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !isCompleted } : todo
+      );
+      setTodoList(updatedTodoList);
+    }
+  };
+
+  const deleteTask = async (id) => {
+    const { error } = await supabase.from("todo").delete().eq("id", id);
+    if (error) {
+      console.log("Error deleting task", error);
+    } else {
+      setTodoList((prev) => prev.filter((todo) => todo.id !== id));
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-[600px] w-full mx-4 p-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Todo List</h1>
+        <div className="flex">
+          <input
+            type="text"
+            placeholder="Add a new todo"
+            className="border border-gray-300 rounded-md p-2 mr-2 flex-1"
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button
+            className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 active:bg-blue-700 transition duration-200"
+            onClick={addTodo}
+          >
+            Add Todo
+          </button>
+        </div>
+        {todoList.length > 0 &&
+          todoList.map((todo) => (
+            <div
+              key={todo.id}
+              className="flex  justify-between mt-4 border border-gray-300 rounded-md p-4 bg-gray-50 mb-4"
+            >
+              <div className="flex flex-col">
+                <p className="text-lg font-medium">{todo.name}</p>
+                <p
+                  className={`text-sm ${
+                    todo.isCompleted ? "text-green-500" : "text-red-500"
+                  }`}
+                >
+                  {todo.isCompleted ? "Completed" : "Not Completed"}
+                </p>
+              </div>
+              <div className="flex flex-col items-end">
+                <button
+                  className=" mt-2  bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 active:bg-blue-700 transition duration-200"
+                  // onClick={() => toggleComplete(todo.id, todo.isCompleted)}
+                  onClick={() => completeTask(todo.id, todo.isCompleted)}
+                >
+                  {todo.isCompleted ? "Mark Incomplete" : "Mark Complete"}
+                </button>
+                <button
+                  className=" mt-2  bg-red-500 text-white rounded-md px-4 py-2 hover:bg-red-600 active:bg-red-700 transition duration-200"
+                  // onClick={() => deleteTask(todo.id)}
+                  onClick={() => deleteTask(todo.id)}
+                >
+                  Delete Task
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
